b5347aafa703367893465fd7d5ff60c182dc8a37
Peter Rwatschew <pete.rwatschew@bbc.co.uk>
2020-10-05 09:10:39 +0100

    remove unwanted files

--- app/src/main/java/com/example/basic_iplayer/Category.kt
+++
@@ -1,3 +0,0 @@
-package com.example.basic_iplayer
-
-data class Category(val id: String = "", val title: String = "", val type: String = "", val kind: String = "")
--- app/src/main/java/com/example/basic_iplayer/iPlayerReqObj.kt
+++ app/src/main/java/com/example/basic_iplayer/iPlayerReqObj.kt
@@ -1,25 +1,5 @@
 package com.example.basic_iplayer
 
-data class iPlayerReqObj(val version: String ="", val schema: String = "", val categories: Array<Category> = arrayOf(Category())) {
+data class iPlayerReqObj(val version: String ="", val schema: String = "", val categories: Array<Category> = arrayOf(Category()))
 
-    //overrides were created by IDE automatically
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as iPlayerReqObj
-
-        if (version != other.version) return false
-        if (schema != other.schema) return false
-        if (!categories.contentEquals(other.categories)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = version.hashCode()
-        result = 31 * result + schema.hashCode()
-        result = 31 * result + categories.contentHashCode()
-        return result
-    }
-}
+data class Category(val id: String = "", val title: String = "", val type: String = "", val kind: String = "")
